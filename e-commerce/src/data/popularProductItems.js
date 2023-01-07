e98983099c6296b3ed50ff9eaf4ae2f1666325d3
Tserenpuntsag Batchuluun <b.tserenpuntsag@Tserenpuntsags-MacBook-Air.local>
2023-01-07 22:50:27 +0800

    Popular Product Slider

---
+++ e-commerce/src/data/popularProductItems.js
@@ -0,0 +1,43 @@
+const PPItems = [
+    {
+        image: "images.images.camera2.png",
+        title: "Camera",
+        price: "$11.70"
+    },
+    {
+        image: "images.images.headphones.png",
+        title: "Wireless Headphones",
+        price: "$11.70"
+    },
+    {
+        image: "images.images.headphones.png",
+        title: "Wireless Headphones",
+        price: "$11.70"
+    },
+    {
+        image: "images.images.game.png",
+        title: "Play Game",
+        price: "$11.70"
+    },
+    {
+        image: "images.images.game.png",
+        title: "Play Game",
+        price: "$11.70"
+    },
+    {
+        image: "images.images.laptop.png",
+        title: "NoteBook",
+        price: "$11.70"
+    },
+    {
+        image: "images.images.camera2.png",
+        title: "Play Game",
+        price: "$11.70"
+    },
+    {
+        image: "images.images.laptop.png",
+        title: "NoteBook",
+        price: "$11.70"
+    }
+]
+export default PPItems;
