2a1b9e9c4f63362b32496cca4f8fdb84769e0674
Morning <morning@mstars-lab2-02s-iMac.local>
2023-01-10 09:37:29 +0800

    update

--- e-commerce/src/components/LastSlider.jsx
+++ e-commerce/src/components/LastSlider.jsx
@@ -1,7 +1,7 @@
 export default function LastSlider(props) {
     return (
         <div style={{display:"flex"}}>
-            <div style={{width:"650px", height:"369px", display: "flex", justifyContent: "space-between", alignItems: "center", border: '1px solid #C8C8C8', borderRadius: "20px", padding:"40px 17px" }}>
+            <div style={{ width:"650px  ",height:"369px", display: "flex", justifyContent: "space-between", alignItems: "center", border: '1px solid #C8C8C8', borderRadius: "20px", padding:"40px 17px" , margin:"0 5px"}}>
                 <img src={props.image} alt="" />
                 <div style={{width:"265px"}}>
                     <div style={{ width: '147px', height: "41p", border: "1px solid #C8C8C8", borderRadius: "20px", color: "#1B5A7D", fontSize: "20px", display:"flex", justifyContent:"center", alignItems:"center" }}>{props.date}</div>
